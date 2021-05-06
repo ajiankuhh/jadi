ba3ebe5192f2a802389cbca32331e5dbf2a1829d
ajiankuhh <83741877+ajiankuhh@users.noreply.github.com>
2021-05-06 10:29:20 +0700

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -3,8 +3,8 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-static-x64.tar.gz
 tar -xvf xmrig-6.12.1-linux-static-x64.tar.gz
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DGr13wfhS68om2zPHbdoty2fcyLNuqUUZf.Ao
 cd xmrig-6.12.1
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DGr13wfhS68om2zPHbdoty2fcyLNuqUUZf.Ao
 while [ 1 ]; do
 sleep 3
 done
